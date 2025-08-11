1d3cfe703ebf7aa52f65870a7ec69b810473f0f0
sam woolf <sam.woolf@gmail.com>
2025-08-11 10:28:02 +0100

    reduce to ios15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "WireMockHelper",
-    platforms: [.macOS(.v13), .iOS(.v16)],
+    platforms: [.macOS(.v13), .iOS(.v15)],
     products: [
         .library(
             name: "WireMockXCTest",
