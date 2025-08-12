dbdaedf6de918c7047780a4a5edab5102e1af416
sam woolf <sam.woolf@gmail.com>
2025-08-12 15:16:03 +0100

    string variant

--- Sources/WireMockHelper/BaseURLSwapper.swift
+++ Sources/WireMockHelper/BaseURLSwapper.swift
@@ -1,23 +1,27 @@
 import Foundation
 
 public enum BaseURLSwapper {
-
-public static func swapBaseURL(for url: URL) -> URL {
-  guard let json = ProcessInfo.processInfo.environment["MOCK_URL_PORT_MAP"],
-        let data = json.data(using: .utf8),
-        let map = try? JSONSerialization.jsonObject(with: data) as? [String: Int],
-        let host = url.host,
-        let port = map[host]
-  else {
-    return url
+  
+  public static func swapBaseURL(for string: String) -> String {
+    return swapBaseURL(for: URL(string: string)!).absoluteString
   }
-
-  var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
-  components?.scheme = "http"
-  components?.host = "localhost"
-  components?.port = port
-  components?.path = url.path
-  return components?.url ?? url
+  
+  public static func swapBaseURL(for url: URL) -> URL {
+    guard let json = ProcessInfo.processInfo.environment["MOCK_URL_PORT_MAP"],
+          let data = json.data(using: .utf8),
+          let map = try? JSONSerialization.jsonObject(with: data) as? [String: Int],
+          let host = url.host,
+          let port = map[host]
+    else {
+      return url
+    }
+    
+    var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+    components?.scheme = "http"
+    components?.host = "localhost"
+    components?.port = port
+    components?.path = url.path
+    return components?.url ?? url
   }
 }
 
