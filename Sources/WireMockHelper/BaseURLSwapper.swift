0d4d6013887faa628b46229906c073257b134d66
sam woolf <sam.woolf@gmail.com>
2025-08-13 13:01:23 +0100

    update urls in dat adictionary

--- Sources/WireMockHelper/BaseURLSwapper.swift
+++ Sources/WireMockHelper/BaseURLSwapper.swift
@@ -2,6 +2,30 @@ import Foundation
 
 public enum BaseURLSwapper {
 
+  private static func jsonToDictionary(_ json: Data) throws -> [String: Any] {
+    let object = try JSONSerialization.jsonObject(with: json, options: [])
+    guard let dictionary = object as? [String: Any] else {
+      throw NSError(domain: "InvalidJSON", code: 0, userInfo: [NSLocalizedDescriptionKey: "Root is not a dictionary"])
+    }
+    return dictionary
+  }
+
+  public static func swapBaseURLs(in data: Data) throws -> Data {
+    var dictionary = try jsonToDictionary(data)
+    for (key, value) in dictionary {
+      if let stringValue = value as? String {
+        dictionary[key] = swapBaseURL(for: stringValue) as Any
+      }
+      if var stringsValue = value as? [String] {
+        stringsValue = stringsValue.map { swapBaseURL(for: $0)}
+        dictionary[key] = stringsValue as Any
+      }
+    }
+
+    return try JSONSerialization.data(withJSONObject: dictionary, options: [])
+  }
+
+
   public static func swapBaseURL(for string: String) -> String {
     guard let url = URL(string: string) else {
       return string
@@ -19,7 +43,6 @@ public enum BaseURLSwapper {
       print("didn't find a match for \(url)")
       return url
     }
-
     var components = URLComponents(url: url, resolvingAgainstBaseURL: false)
     components?.scheme = "http"
     components?.host = "localhost"
