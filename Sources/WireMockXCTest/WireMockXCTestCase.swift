a16506531df89426dec2fd0ee1316d51a4835d20
sam woolf <sam.woolf@gmail.com>
2025-08-28 17:47:30 +0100

    remove record option

--- Sources/WireMockXCTest/WireMockXCTestCase.swift
+++ Sources/WireMockXCTest/WireMockXCTestCase.swift
@@ -1,9 +1,5 @@
+import Foundation
 import XCTest
-
-public enum Mode {
-  case record
-  case playback
-}
 
 open class WireMockXCTestCase: XCTestCase {
 
@@ -13,10 +9,9 @@ open class WireMockXCTestCase: XCTestCase {
 
   public var app: XCUIApplication!
   public var wireMocks: [WireMock] = []
-  public var mode: Mode = .playback
   public var hostMock: WireMock!
 
-  public func setUp(mode: Mode = .playback) {
+  public override func setUp() {
     super.setUp()
     guard let url = Bundle(for: type(of: self)).url(forResource: "wiremock_config", withExtension: "json") else {
       XCTFail("Could not find wiremock_config.json in test bundle")
@@ -36,115 +31,7 @@ open class WireMockXCTestCase: XCTestCase {
     self.hostMock = hostMock
     self.app = XCUIApplication()
     app.launchEnvironment["CONFIG_BASE_URL"] = "http://localhost:\(hostMock.port)"
-    self.mode = mode
-
-    if mode == .record {
-      addTeardownBlock {
-        let _ = await withTaskGroup(of: Bool.self) { group in
-          for mock in self.wireMocks {
-            group.addTask {
-              let result = await mock.stopRecording()
-              switch result {
-              case .success:
-                print("✅ \(mock.name) recording stopped")
-                return true
-              case .failure(let error):
-                print("❌ \(mock.name) failed to stop: \(error)")
-                return false
-              }
-            }
-          }
-          var success = true
-          for await result in group {
-            if !result {
-              success = false
-            }
-          }
-          return success
-        }
-      }
-      Task {
-        await startRecordingAndLaunchApp()
-      }
-    } else {
-      Task {
-       // let _ = await stubConfigURL(wireMock: hostMock)
-        await self.app.launch()
-      }
-    }
     continueAfterFailure = false
-  }
-
-  private func stubConfigURL(wireMock: WireMock) async -> Result<Void, Swift.Error> {
-    guard let url = Bundle(for: type(of: self)).url(forResource: "config", withExtension: "json"),
-          let configOverride = wireMock.configOverride else {
-      return .failure(Error.noConfigFile)
-    }
-    do {
-      let jsonString = try String(contentsOf: url, encoding: .utf8)
-      return await withTaskGroup(of: Result<Void, Swift.Error>.self) { group in
-        group.addTask {
-          await wireMock.stubbedResponse(
-            request: .init(method: configOverride.method, urlPath: configOverride.endpoint),
-            response: .init(body: jsonString)
-          )
-        }
-        for await result in group {
-          if case .failure = result {
-            return result
-          }
-        }
-        return .success(())
-      }
-    }catch {
-      XCTFail("failed to read config file")
-      return .failure(error)
-    }
-  }
-
-  private func startRecordingAndLaunchApp() async {
-    guard !wireMocks.isEmpty else { return }
-    let allSucceeded = await withTaskGroup(of: Bool.self) { group in
-      group.addTask {
-        let result = await self.stubConfigURL(wireMock: self.hostMock)
-        switch result {
-        case .success:
-          print("✅ stubbing config")
-          return true
-        case .failure(let error):
-          print("❌  failed to stuub config: \(error)")
-          return false
-        }
-      }
-      for mock in wireMocks {
-        group.addTask {
-          let result = await mock.startRecording()
-          switch result {
-          case .success:
-            print("✅ \(mock.name) recording started")
-            return true
-          case .failure(let error):
-            print("❌ \(mock.name) failed to start: \(error)")
-            return false
-          }
-        }
-      }
-      var success = true
-      for await result in group {
-        if !result {
-          success = false
-        }
-      }
-      return success
-    }
-
-    if allSucceeded {
-      print("🎬 All WireMock Servers recording. Launching app.")
-      await app.launch()
-    } else {
-      print("🚫 Some WireMock servers failed to start recording..")
-      XCTFail()
-    }
   }
 }
 
